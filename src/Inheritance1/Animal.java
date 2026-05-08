27a069174e903c24d406ac91d030b129adc7fd0b
Ed <leonardo071123@gmail.com>
2026-05-08 11:09:42 +0800

    update: 繼承筆記

--- src/Inheritance1/Animal.java
+++ src/Inheritance1/Animal.java
@@ -2,8 +2,9 @@ package Inheritance1;
 
 public class Animal extends Living{
     private String name;
-
+    // 因為繼承了Living 所以Animal的建構子會呼叫父類別的建構子
     public Animal(String name, String type) {
+        // super(): 呼叫父類別建構子
         super(type);
         setName(name);
         System.out.println("Animal 建構子執行：名字是 " + getName());
