f4b4a55a7e5a625a303541ed34e1e5d161ad59b9
Ed <leonardo071123@gmail.com>
2026-05-08 11:10:32 +0800

    繼承筆記

--- src/Inheritance1/Dog.java
+++ src/Inheritance1/Dog.java
@@ -3,7 +3,9 @@ package Inheritance1;
 public class Dog extends Animal{
     private String breed;
 
+    // 因為繼承了Animal 所以Dog的建構子會呼叫父類別的建構子
     public Dog(String type, String name, String breed) {
+        // super(): 呼叫父類別建構子
         super(name, type);
         setBreed(breed);
         System.out.println("Dog 建構子執行：品種是 " + getBreed());
